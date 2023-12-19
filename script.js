ea14f2fd87c130d4690987cdc6f3c72f5b27e3b0
Muhammadu Barry <muhammadubarry5@gmail.com>
2023-12-19 14:28:46 -0500

    update

--- script.js
+++ script.js
@@ -45,17 +45,20 @@ const decimalButtonSecondNumber = (e) => {
 };
 
 // this listens for our operator and only lets us click it once then it starts our secondNumber
+let operator;
+
 const operatorRecord = (e) => {
-    console.log("operator:");
-      console.log(e.target.value);
-      for (let i = 0; i < operatorButton.length; i++) {
-        operatorButton[i].removeEventListener('click', operatorRecord);
-      }
-      for (let i = 0; i < numbers.length; i++) {
-        numbers[i].removeEventListener("click", firstNumberStringRecord);
-      }
-      secondNumber();
-}
+  console.log("operator:");
+  console.log(e.target.value);
+  for (let i = 0; i < operatorButton.length; i++) {
+    operatorButton[i].removeEventListener("click", operatorRecord);
+  }
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i].removeEventListener("click", firstNumberStringRecord);
+  }
+  secondNumber();
+  operator = e.target.getAttribute("id");
+};
 
 //this is our operator that contains our first number and our Second
 const operation = () => {
@@ -64,5 +67,44 @@ const operation = () => {
   }
 };
 
+// this button listens for our equal button
+const listenEqual = () => {
+  equalButton.addEventListener("click", () => {
+    firstNumberString = parseFloat(firstNumberString);
+    secondNumberString = parseFloat(secondNumberString);
+    reset();
+  });
+};
+
+// we need to reset our function once we obtain our results
+const reset = () => {
+  firstNumberString = mathFunction(operator);
+  console.log("result:", firstNumberString);
+  secondNumberString = "";
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i].removeEventListener("click", secondNumberStringRecord);
+  }
+  operation();
+};
+
+// this function takes our 2 numbers and does the math accordingly
+const mathFunction = (id) => {
+  switch (id) {
+    case "remainder":
+      return firstNumberString % secondNumberString;
+    case "divide":
+      return firstNumberString / secondNumberString;
+    case "add":
+      return firstNumberString + secondNumberString;
+    case "multiplication":
+      return firstNumberString * secondNumberString;
+    case "subtraction":
+      return firstNumberString - secondNumberString;
+    default:
+      console.log("OOPS SOMETHING WENT WRONG");
+  }
+};
+
 firstNumber();
 operation();
+listenEqual();
